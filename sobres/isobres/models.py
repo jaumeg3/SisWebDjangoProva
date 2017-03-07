ea45cb307037cc730ed8a2486b591d405eb1cd6f
Jaume Giralt Barbé <jaume.giralt.2012@gmail.com>
2017-03-07 12:48:08 +0100

    View Done

--- sobres/isobres/models.py
+++ sobres/isobres/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Empresa(models.Model):
+
+    """docstring for ."""
+    nom = models.TextField(max_length=50)
+    web = models.TextField(max_length=100)
+
+
+class Sobre(models.Model):
+
+    """docstring fordddddddd"""
+    amount = models.IntegerField()
+    date = models.DateTimeField()
+    concept = models.TextField(max_length=50)
+    empresa = models.ForeignKey(Empresa)
+    politic = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.empresa.nom + ":" + str(self.amount) + ":"
+        + self.politic.get_username()
--- sobres/isobres/views.py
+++ sobres/isobres/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def mainpage(request):
+    page = """
+    <html>
+    <body>
+    Sobres, a PP Application.
+    </body>
+    </html>
+    """
+
+    return HttpResponse(page)
